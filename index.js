8fd67c9b4cf96de4d297c947e98c8fb9c84e7d8d
Bara Sevcikova <bara.sevcikova@gmail.com>
2022-05-16 22:04:18 +0200

    Hop

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("moc nečum");
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
